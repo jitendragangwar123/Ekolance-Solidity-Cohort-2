aa748dc0bedf268c62e443ae375ea80807907c99
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-11-03 16:32:40 +0530

    initial commit

--- Week-1/Assignment/EtherWallet.sol
+++ Week-1/Assignment/EtherWallet.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract EtherWallet {
     address payable public owner;
+    mapping(address => uint) private sender_amount;
 
     constructor() {
         owner = payable(msg.sender);
@@ -11,12 +12,26 @@ contract EtherWallet {
     // receive() external payable {}
 
     function deposit() external payable {
+        require(msg.value > 0,"amount must be greater than zero!");
+        sender_amount[msg.sender]+=msg.value;
         
     }
 
     function withdraw(uint _amount) external {
         require(msg.sender == owner, "caller is not owner");
         payable(msg.sender).transfer(_amount);
+    }
+
+    function withdrawDeposit() public{
+        address sender_address =payable(msg.sender);
+        uint amount=sender_amount[sender_address];
+        (bool success,)=sender_address.call{value:amount}("Withdraw");
+        require(success,"Withdraw not successful!");
+        sender_amount[sender_address]=0;
+    }
+
+    function getDepositAmount() public view returns(uint){
+        return sender_amount[msg.sender];
     }
 
     function getBalance() external view returns (uint) {
