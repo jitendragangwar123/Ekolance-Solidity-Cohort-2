b7002699fa3074e033958b3b43f3e03fc0a794da
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-11-07 16:13:12 +0530

    initial commit

--- Week-2/15_Library.sol
+++ Week-2/15_Library.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.16;
 
 /*
 Libraries are similar to contracts, but you can't declare any state variable and you can't send ether.
