b2ee426a75ee003227de31a4eca26dc8c52bd0fe
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-08-04 23:40:00 +0530

    feat:block timestamp manipulation in solidity

---
+++ Week-4/6_Block_Timestamp_Manipulation.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Roulette {
+    uint256 public pastBlockTime;
+
+    constructor() payable {}
+
+    function spin() external payable {
+        require(msg.value == 10 ether); // must send 10 ether to play
+        require(block.timestamp != pastBlockTime); // only 1 transaction per block
+
+        pastBlockTime = block.timestamp;
+
+        if (block.timestamp % 15 == 0) {
+            (bool sent,) = msg.sender.call{value: address(this).balance}("");
+            require(sent, "Failed to send Ether");
+        }
+    }
+}
