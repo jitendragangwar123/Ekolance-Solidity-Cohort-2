df440d29d263d57f9c104a0f2e50f958a172dee2
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2025-04-28 14:15:52 +0530

    chore:content formatting

--- Week-1/10_GasAndEther.sol
+++ Week-1/10_GasAndEther.sol
@@ -1,39 +1,20 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.15;
-contract EtherUnits {
-    uint public oneWei = 1 wei;
-    // 1 wei is equal to 1
-    bool public isOneWei = 1 wei == 1;
-
-    uint public oneEther = 1 ether;
-    // 1 ether is equal to 10^18 wei
-    bool public isOneEther = 1 ether == 1e18;
-}
-
-
 /*
-
-
 How much ether do you need to pay for a transaction?
-
-You pay gas spent * gas price amount of ether, where
-
+    - You pay gas spent * gas price amount of ether, where
     - gas is a unit of computation
     - gas spent is the total amount of gas used in a transaction
     - gas price is how much ether you are willing to pay per gas
-
-    Transactions with higher gas price have higher priority to be included in a block.
-
+    - Transactions with higher gas price have higher priority to be included in a block.
     - Unspent gas will be refunded.
 
-    Gas Limit
-
-There are 2 upper bounds to the amount of gas you can spend
-
+Gas Limit:-
+    There are 2 upper bounds to the amount of gas you can spend:
     - gas limit (max amount of gas you're willing to use for your transaction, set by you)
     - block gas limit (max amount of gas allowed in a block, set by the network)
-
 */
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.24;
 
 contract Gas {
     uint public i = 0;
@@ -48,4 +29,14 @@ contract Gas {
             i += 1;
         }
     }
+}
+
+contract EtherUnits {
+    uint public oneWei = 1 wei;
+    // 1 wei is equal to 1
+    bool public isOneWei = 1 wei == 1;
+
+    uint public oneEther = 1 ether;
+    // 1 ether is equal to 10^18 wei
+    bool public isOneEther = 1 ether == 1e18;
 }
