010b7d92d6310884a05ffd7e637f1d4b5bac9760
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2025-04-29 09:31:40 +0530

    chore:content formatting

--- Week-2/1_Data_Location.sol
+++ Week-2/1_Data_Location.sol
@@ -1,43 +1,40 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
-
 /*
+ - Variables are declared as either storage, memory or calldata to explicitly specify the location of the data.
 
-Variables are declared as either storage, memory or calldata to explicitly specify the location of the data.
+ 1. storage - variable is a state variable (store on blockchain)
+ 2. memory - variable is in memory and it exists while a function is being called
+ 3. calldata - special data location that contains function arguments
 
-storage - variable is a state variable (store on blockchain)
-memory - variable is in memory and it exists while a function is being called
-calldata - special data location that contains function arguments
+1. storage
+ - It can be used for both function declaration parameters as well as within the function logic
+ - It is mutable (it can be overwritten and changed)
+ - It is persistent (the value persists on the blockchain)
 
-
-storage
-It can be used for both function declaration parameters as well as within the function logic
-It is mutable (it can be overwritten and changed)
-It is persistent (the value persists on the blockchain)
-
-memory
-It can be used for both function declaration parameters as well as within the function logic
-It is mutable (it can be overwritten and changed)
-It is non-persistent (the value does not persist after the transaction has completed)
+2. memory
+ - It can be used for both function declaration parameters as well as within the function logic
+ - It is mutable (it can be overwritten and changed)
+ - It is non-persistent (the value does not persist after the transaction has completed)
 
 
-callData
-It can only be used for function declaration parameters (and not function logic)
-It is immutable (it can't be overwritten and changed)
-It must be used for dynamic parameters of an external function
-It is non-persistent (the value does not persist after the transaction has completed)
+3. callData
+ - It can only be used for function declaration parameters (and not function logic)
+ - It is immutable (it can't be overwritten and changed)
+ - It must be used for dynamic parameters of an external function
+ - It is non-persistent (the value does not persist after the transaction has completed)
 */
 
 
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
 contract DataLocations {
-    uint[] public arr;
-    mapping(uint => address) map;
+    uint256[] public arr;
+    mapping(uint256 => address) map;
     struct MyStruct {
-        uint foo;
+        uint256 foo;
     }
-    mapping(uint => MyStruct) myStructs;
+    mapping(uint256 => MyStruct) myStructs;
 
     function f() public {
         // call _f with state variables
@@ -50,41 +47,44 @@ contract DataLocations {
     }
 
     function _f(
-        uint[] storage _arr,
-        mapping(uint => address) storage _map,
+        uint256[] storage _arr,
+        mapping(uint256 => address) storage _map,
         MyStruct storage _myStruct
     ) internal {
         // do something with storage variables
     }
 
     // You can return memory variables
-    function g(uint[] memory _arr) public returns (uint[] memory) {
+    function g(uint256[] memory _arr) public returns (uint256[] memory) {
         // do something with memory array
     }
 
-    function h(uint[] calldata _arr) external {
+    function h(uint256[] calldata _arr) external {
         // do something with calldata array
     }
+}
 
- // example
-
-// callData vs Memory in solidity 
-
-    contract Test {
-    
+contract Test {
     string stringTest;
-    
-    function memoryTest(string memory _exampleString) public returns (string memory) {
-        _exampleString = "example";  // You can modify memory
-        string memory newString = _exampleString;  // You can use memory within a function's logic
-        return newString;  // You can return memory
+
+    function memoryTest(string memory _exampleString)
+        public
+        pure
+        returns (string memory)
+    {
+        // You can modify memory
+        _exampleString = "example"; 
+        // You can use memory within a function's logic
+        string memory newString = _exampleString; 
+        return newString; 
     }
-    
-    function calldataTest(string calldata _exampleString) external returns (string memory) {
-        // cannot modify _exampleString
-        // but can return it
+
+    function calldataTest(string calldata _exampleString)
+        external
+        pure
+        returns (string memory)
+    {
+        // cannot modify _exampleString, but can return it
         return _exampleString;
     }
 }
-}
-
