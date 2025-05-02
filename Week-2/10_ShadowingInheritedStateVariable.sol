62440fa694eeadb47853648855df3df15a19cfe3
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2025-05-02 09:24:28 +0530

    chore:content formatting

--- Week-2/10_ShadowingInheritedStateVariable.sol
+++ Week-2/10_ShadowingInheritedStateVariable.sol
@@ -1,13 +1,10 @@
-// SPDX-License-Identifier: MIT
-
-
 /*
-Unlike functions, state variables cannot be overridden by re-declaring it in the child contract.
-
-Let's learn how to correctly override inherited state variables.
+ - Unlike functions, state variables cannot be overridden by re-declaring it in the child contract.
+ - Let's learn how to correctly override inherited state variables.
 */
 
 
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 contract A {
@@ -18,11 +15,13 @@ contract A {
     }
 }
 
-// Shadowing is disallowed in Solidity 0.6
-// This will not compile
-// contract B is A {
-//     string public name = "Contract B";
-// }
+/*
+    - Shadowing is disallowed in Solidity 0.6
+    - This will not compile
+        contract B is A {
+            string public name = "Contract B";
+        }
+*/
 
 contract C is A {
     // This is the correct way to override inherited state variables.
